0d6ee692a6804067f90caeb5940cb6612698d836
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-06-24 00:42:42 +0200

    Modification des liens relatif des fichiers css et js quite à la configuration rewrite ajouté au htacces sur le serveur

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,7 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . "../vendor/autoload.php");
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <base href="https://jean-ely-gendrau.students-laplateforme.io/autocompletion/" />
   <title>Autocompletion PHP - JS</title>
-  <link rel="stylesheet" href="./assets/styles/theme.css">
+  <link rel="stylesheet" href="https://jean-ely-gendrau.students-laplateforme.io/autocompletion/public_html/assets/styles/theme.css">
 </head>
 
 <body class="text-sm text-gray-900 border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
@@ -19,7 +19,7 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . "../vendor/autoload.php");
   </main>
 
   <!-- ADD SCRIPT -->
-  <script src="./script.js"></script>
+  <script src="https://jean-ely-gendrau.students-laplateforme.io/autocompletion/public_html/script.js"></script>
 </body>
 
 </html>
