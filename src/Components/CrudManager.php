0556a8247e6739c6c6cee814be79b118887a2f77
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-06-25 07:51:49 +0200

    Remplacement de la requête sql getByLike , celon la version de MariaDB 5.6  du server plesk

--- src/Components/CrudManager.php
+++ src/Components/CrudManager.php
@@ -66,8 +66,10 @@ class CrudManager extends BddConnect
    */
   public function getByLike(mixed $search, string $columnLike): array
   {
-    $req = $this->_dbConnect->prepare("SELECT * FROM " . $this->_tableName . " WHERE {$columnLike} LIKE :search AND JSON_CONTAINS(`jsonFood`, :json) LIMIT 10");
-    //$req = $this->_dbConnect->prepare("SELECT * FROM {$this->_tableName}  WHERE {$columnLike} LIKE :search AND JSON_SEARCH(JSON_EXTRACT(`jsonFood`, :json)) LIMIT 10");
+    // SQL ^5.7 req = $this->_dbConnect->prepare("SELECT * FROM " . $this->_tableName . " WHERE {$columnLike} LIKE :search AND JSON_CONTAINS(`jsonFood`, :json) LIMIT 10");
+    $req = $this->_dbConnect->prepare("SELECT *, SUBSTRING_INDEX(SUBSTRING_INDEX(`jsonFood`, '\"nutriscore_grade\": \"', -1), '\", ', 1) AS product_type
+            from foodsopens
+            having product_type = a");
 
     $req->execute($search);
     $req->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, $this->_objectClass);
