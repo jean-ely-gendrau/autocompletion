16b3db49170352460f201670bc3c1886a863a013
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-06-25 22:52:55 +0200

    Ajout de la variable  au tableau de paramètre de requête préparer

--- src/Components/CrudManager.php
+++ src/Components/CrudManager.php
@@ -66,11 +66,11 @@ class CrudManager extends BddConnect
    */
   public function getByLike(mixed $search, string $columnLike): array
   {
-    $arraySelectSql = ['valueSelected' => 'a', 'itemSelectedStart' => '\"nutriscore_grade\": \"', 'itemSelectedEnd' => '\", '];
+    $arraySelectSql = ['valueSelected' => $search, 'itemSelectedStart' => '\"nutriscore_grade\": \"', 'itemSelectedEnd' => '\", '];
     // SQL ^5.7 req = $this->_dbConnect->prepare("SELECT * FROM " . $this->_tableName . " WHERE {$columnLike} LIKE :search AND JSON_CONTAINS(`jsonFood`, :json) LIMIT 10");
     $req = $this->_dbConnect->prepare('SELECT *, SUBSTRING_INDEX(SUBSTRING_INDEX(`jsonFood`, :itemSelectedStart, -1), :itemSelectedEnd, 1) AS item_selected
           from foodsopens
-            having item_selected = :valueSelected LIMIT 10');
+            having item_selected = :valueSelected LIMIT 6');
     // SQL ^5.7 $req->execute($search);
     $req->execute($arraySelectSql);
     $req->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, $this->_objectClass);
